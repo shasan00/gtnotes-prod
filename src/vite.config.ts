9f449bf425edb173d17605b518f3da7ef7332942
shasan00 <shasan3499@gmail.com>
2025-08-15 17:39:48 -0400

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "."),
+      "@": path.resolve(__dirname, "./"),
     },
   },
   // Add this build configuration
